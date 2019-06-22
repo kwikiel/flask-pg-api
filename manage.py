246569845ba0373a869b538e87065780a14ac2a0
alexgnatrow <lesha.gnat@gmail.com>
2019-06-22 21:18:13 +0300

    PoW: get rid of all migrations

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from src import app, db, models
-from src.models import User
+# from src.models import User
+from sqlalchemy.exc import InvalidRequestError
 from psycopg2.errors import UndefinedTable
 import os
 
@@ -14,28 +15,30 @@ migrate = Migrate(app, db)
 # Add the flask migrate
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def init():
-    try:
-        print(User.get_by_id(0))
-    except UndefinedTable:
-        print("No 'users' table")
-    except:
-        db.create_all()
-        db.session.commit()
+    print('in manage')
+    # try:
+    #     print(User.get_by_id(0))
+    # except UndefinedTable:
+    #     print("No 'users' table")
+    # except InvalidRequestError:
+    #     print('Most likely users table already created')
+    # except:
+    #     print("'users' table is not found. Creating it with test data.")
+        # # db.create_all()
+        # # db.session.commit()
 
-        admin = User('admin', 'admin@example.com')
-        guest = User('guest', 'guest@example.com')
-        db.session.add(admin)
-        db.session.add(guest)
-        db.session.commit()
-        users = User.query.all()
-        print (users)
+        # admin = User('admin', 'admin@example.com')
+        # guest = User('guest', 'guest@example.com')
+        # db.session.add(admin)
+        # db.session.add(guest)
+        # db.session.commit()
+        # users = User.query.all()
+        # print (users)
     # Create a user if they do not exist.
-    # user = User.query.filter_by(email="example@bucketmail.com").first()
-    # if not user:
-    #     user = User("example@bucketmail.com", "123456")
-    #     user.save()
+
 
 # Run the manager
 if __name__ == '__main__':
